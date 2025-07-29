f0cda3fa1690979171df375d7e7205e7b8c78534
Davis <daviskv@gmail.com>
2025-07-29 23:44:08 +0530

    nltk download issue"

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-
+nltk.download('punkt_tab')
 
 class SentimentRecommenderModel:
 
